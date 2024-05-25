ea27933890caae855e8aca1a23273695c5a8358c
AlexIGN <84693562+AlexeyIGN@users.noreply.github.com>
2024-05-26 00:16:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 1017890038 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control, test) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    statistic, p_value = ttest_ind(control, test, alternative = 'greater')
+    mean = 4563.65
+    var = 23067579472.51
+    alpha = 0.01
+    
+    if p_value < 0.01:
+        return True
+    else:
+        return False
